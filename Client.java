4e280f50bd304d532dca2fe84c5c9e8a0f9a0318
Lukas <lukas.krasauskis123@gmail.com>
2022-02-19 19:20:26 +0000

    Client can see previous post history

--- Client.java
+++ Client.java
@@ -1,5 +1,7 @@
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 class Client {
     public static void main(String[] args) throws Exception {
@@ -10,14 +12,33 @@ class Client {
         try {
             Socket s = new Socket(host, port);
             DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            while(dis.available() > 0){
+                System.out.println(dis.readUTF());
+                System.out.println("**************************");
+            }
 
-            // Creating a message
+            // Asking user if they want to post a message
+            System.out.println("Would you like to post a message (y/n)? ");
+            Scanner sc = new Scanner(System.in);
+            String userText = sc.next();
+            sc.close();
+            if(userText.equals("y")  || userText.equals("yes")){
 
-            dos.writeUTF(userid);
-            /*
-            Encrypt here
-             */
-            dos.writeUTF("Happy me is");
+                // Creating a message
+                dos.writeUTF(userid);
+                /*
+                Encrypt here
+                 */
+                dos.writeUTF("Happy me is");
+            }
+            else if(userText.equals("n") || userText.equals("no")){
+                System.out.println("Connection Closed");
+                s.close();
+            } else {
+                System.out.println("Unsupported action detected, Terminating connection");
+                s.close();
+            }
 
         } catch (Exception e) {
             System.err.println("Cannot connect to server.");
--- Server.java
+++ Server.java
@@ -1,4 +1,6 @@
 import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -8,16 +10,30 @@ class Server {
 
     public static void main(String[] args) throws Exception {
         Integer port = Integer.parseInt(args[0]);
-
+        ArrayList<String> postHistory = new ArrayList<String>(); // Keeps Track of all posts
         ServerSocket ss = new ServerSocket(port);
         System.out.println("Waiting for incoming connections...");
 
+
         while(true){
             Socket s = ss.accept();
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
             DataInputStream dis = new DataInputStream(s.getInputStream());
+
+            /*
+            Connection Received Send all appropriate data
+             */
+            if(!postHistory.isEmpty())
+                for(String i: postHistory ){
+                    dos.writeUTF(i);
+                }
+
+            //TODO: Decrypt messages
+
+
+
             boolean firstMessage = true;
             String sender = "dummy";
-            ArrayList postHistory = new ArrayList<String>(); // Keeps Track of all posts
             String itext; // Incoming Text
             try {
                 while (( itext = dis.readUTF()) != null) {
@@ -27,9 +43,12 @@ class Server {
                         firstMessage = false;
                     } else {
                         // User Message
-                        String msg = "Sender: " + sender + "\n" +
+                        String msg =
+                                "**************************\n" +
+                                "Sender: " + sender + "\n" +
                                 "Date: " + new java.util.Date() + "\n" +
-                                "Message: " + itext;
+                                "Message: " + itext +
+                                "\n**************************";
 
                         System.out.println(msg);
                         postHistory.add(msg);
@@ -38,7 +57,7 @@ class Server {
                 }
             }
             catch(IOException e) {
-                System.err.println("Client closed its connection.");
+                System.err.println("User Disconnected");
             }
         }
     }
