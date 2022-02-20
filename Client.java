a76053c4227b643f5534a8ecbb018cf83eec9832
Lukas <lukas.krasauskis123@gmail.com>
2022-02-20 16:45:16 +0000

    Finished Assignment

--- Client.java
+++ Client.java
@@ -2,15 +2,15 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import java.io.*;
 import java.net.Socket;
-import java.security.Key;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.Signature;
 import java.util.Base64;
 import java.util.Scanner;
 
 class Client {
     public static void main(String[] args) throws Exception {
-        String host = args[0]; // posibily change to integer
+        String host = args[0];
         Integer port = Integer.parseInt(args[1]);
         String userid = args[2];
         Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
@@ -20,20 +20,19 @@ class Client {
             DataOutputStream dos = new DataOutputStream(s.getOutputStream());
             DataInputStream dis = new DataInputStream(s.getInputStream());
 
-            // Reads The private Key
             ObjectInputStream obis = new ObjectInputStream(new FileInputStream(userid+".prv"));
             PrivateKey privateKey = (PrivateKey)obis.readObject();
             obis.close();
 
-            // Read and Decrypt
+            //Reads server post history and tries to decode and decrypt the messages
+
             while(dis.available() > 0){
                 System.out.print(dis.readUTF());
                 String msgText = dis.readUTF();
-                // Decode Base64
-                Base64.Decoder decoder = Base64.getDecoder();
-                byte[] msgEncrypted = decoder.decode(msgText);
-                // Decrypt
                 try{
+                    Base64.Decoder decoder = Base64.getDecoder();
+                    byte[] msgEncrypted = decoder.decode(msgText);
+
                     cipher.init(Cipher.DECRYPT_MODE,privateKey);
                     String decipheredMsg = new String(cipher.doFinal(msgEncrypted),"UTF8");
                     System.out.print(decipheredMsg);
@@ -45,13 +44,13 @@ class Client {
                 System.out.println("\n**************************");
             }
 
-            // Asking user if they want to post a message
             System.out.println("Would you like to post a message (y/n)? ");
             Scanner sc = new Scanner(System.in);
             String userDecision = sc.next();
             if(userDecision.equals("y")  || userDecision.equals("yes")){
 
-                // Taking User Input
+                String date = String.valueOf(new java.util.Date());
+
                 System.out.println("Enter the recipient userid (type \"all\" for posting without encryption):");
                 String recipient = sc.next();
                 System.out.println("Enter your message:");
@@ -59,26 +58,35 @@ class Client {
                 String userMessage = sc.nextLine();
                 sc.close();
 
-                // Getting the public key of the recipient
-                ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(recipient+".pub"));
-                PublicKey publicKey = (PublicKey)objectInputStream.readObject();
-                objectInputStream.close();
+                if(!recipient.equalsIgnoreCase("all")){
+                    ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(recipient+".pub"));
+                    PublicKey publicKey = (PublicKey)objectInputStream.readObject();
+                    objectInputStream.close();
 
-                // Creating a message
+                    cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+                    byte[] encryptText = cipher.doFinal(userMessage.getBytes("UTF8"));
+
+                    Base64.Encoder encoder = Base64.getEncoder();
+                    userMessage = encoder.encodeToString(encryptText);
+
+
+                }
+
                 dos.writeUTF(userid);
-                dos.writeUTF(String.valueOf(new java.util.Date()));
+                dos.writeUTF(date);
+                dos.writeUTF(userMessage);
 
-                // Encrypting the message
-                cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-                byte[] encryptText = cipher.doFinal(userMessage.getBytes("UTF8"));
+                Signature signature = Signature.getInstance("SHA1withRSA");
+                signature.initSign(privateKey);
+                signature.update(userid.getBytes());
+                signature.update(date.getBytes());
+                signature.update(userMessage.getBytes());
+                byte[] signedSignature = signature.sign();
 
-                //BASE64 Coversion
-                Base64.Encoder encoder = Base64.getEncoder();
-                String encodedText = encoder.encodeToString(encryptText);
-                dos.writeUTF(encodedText);
+                dos.writeInt(signedSignature.length);
+                dos.write(signedSignature);
 
 
-                // TODO: Send signature
             }
             else if(userDecision.equals("n") || userDecision.equals("no")){
                 System.out.println("Connection Closed");
@@ -88,10 +96,14 @@ class Client {
                 s.close();
             }
 
+            dis.close();
+            dos.close();
+
+        }catch (FileNotFoundException fileNotFoundException){
+          System.err.println("Missing Keys");
         } catch (Exception e) {
             System.err.println("Cannot connect to server.");
         }
-        // TODO: Expection for missing public key
 
     }
 }
--- Server.java
+++ Server.java
@@ -1,36 +1,28 @@
-import java.io.DataInputStream;
-import java.io.DataOutput;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.security.PublicKey;
+import java.security.Signature;
 import java.util.LinkedHashMap;
-import java.util.Map;
 
 class Server {
 
     public static void main(String[] args) throws Exception {
         Integer port = Integer.parseInt(args[0]);
-        LinkedHashMap<String,String> postHistory = new LinkedHashMap<>(); // Keeps Track of all posts
+        LinkedHashMap <String, String> postHistory = new LinkedHashMap <>(); // Keeps Track of all posts
         ServerSocket ss = new ServerSocket(port);
         System.out.println("Waiting for incoming connections...");
 
 
-        while(true){
+        while (true) {
             Socket s = ss.accept();
             DataOutputStream dos = new DataOutputStream(s.getOutputStream());
             DataInputStream dis = new DataInputStream(s.getInputStream());
 
-            /*
-            Connection Received Send post history
-             */
-            if(!postHistory.isEmpty())
-                for(String key: postHistory.keySet() ){
+            if (!postHistory.isEmpty())
+                for (String key : postHistory.keySet()) {
                     dos.writeUTF(postHistory.get(key));
                     dos.writeUTF(key);
-
                 }
 
             String sender = "dummy";
@@ -38,34 +30,49 @@ class Server {
             String itext;
             String msgText;
             try {
-                while (( itext = dis.readUTF()) != null) {
-                    // The first message is always the userID
-                        sender = itext;
-                        date = dis.readUTF();
-                        msgText = dis.readUTF();
-                        // User Message
-                        String msgOutput =
-                                "**************************\n" +
-                                "Sender: " + sender + "\n" +
-                                "Date: " + date + "\n" +
-                                "Message: " + msgText +
-                                "\n**************************";
+                while ((itext = dis.readUTF()) != null) {
+                    sender = itext;
+                    date = dis.readUTF();
+                    msgText = dis.readUTF();
+                    String msgOutput =
+                            "**************************\n" +
+                                    "Sender: " + sender + "\n" +
+                                    "Date: " + date + "\n" +
+                                    "Message: " + msgText +
+                                    "\n**************************";
 
-                        System.out.println(msgOutput);
-//                        postHistory.add(msgOutput);
+                    System.out.println(msgOutput);
                     // This message is stored in postHistory and will be sent to the next client,
                     // msgText is stored separately to make it easier to access it and decipher.
                     String messageToSend =
                             "**************************\n" +
-                            "Sender: " + sender + "\n" +
-                            "Date: " + date + "\n" +
-                            "Message: ";
-                        postHistory.put(msgText,messageToSend);
+                                    "Sender: " + sender + "\n" +
+                                    "Date: " + date + "\n" +
+                                    "Message: ";
 
+                    ObjectInputStream readKey = new ObjectInputStream(new FileInputStream(sender + ".pub"));
+                    PublicKey publicKey = (PublicKey) readKey.readObject();
+                    readKey.close();
+                    int lenght = dis.readInt();
+                    if (lenght > 0) {
+                        byte[] signature = new byte[lenght];
+                        dis.readFully(signature, 0, signature.length);
 
+                        Signature sig = Signature.getInstance("SHA1withRSA");
+                        sig.initVerify(publicKey);
+                        sig.update(sender.getBytes());
+                        sig.update(date.getBytes());
+                        sig.update(msgText.getBytes());
+                        if(sig.verify(signature)){
+                            System.out.println("Signature Verified");
+                            postHistory.put(msgText, messageToSend);
+                        } else {
+                            System.out.println("Invalid Signature");
+                        }
+
+                    }
                 }
-            }
-            catch(IOException e) {
+            } catch (IOException e) {
                 System.err.println("User Disconnected");
             }
         }
